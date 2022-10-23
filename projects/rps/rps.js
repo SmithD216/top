06fc673163cca7c41d6849e9199660cdc61a7596
D Smith <smithd216@pm.me>
2022-10-23 15:21:53 +0100

    Finished game functionality, needs a rework but it's functional as it is

--- projects/rps/rps.js
+++ projects/rps/rps.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let cpuScore = 0;
 playGame();
 
 function getComputerChoice(){
@@ -19,36 +21,63 @@ function playRound(player, cpu){
     let result = "";
     if(player.toLowerCase() == "rock"){
         if(cpu == "rock"){
+            playerScore++;
+            cpuScore++;
             result = "It's a draw you both picked rock!";
         }else if(cpu == "paper"){
-            result = "You lose! Paper beats rock!"
+            cpuScore++;
+            result = "You lose! Paper beats rock!";
         }else{
-            result = "You win! Rock beats scissors!"
+            playerScore++;
+            result = "You win! Rock beats scissors!";
         }
     } else if(player == "paper"){
         if(cpu == "rock"){
+            playerScore++;
             result = "You win! Paper beats rock!";
         }else if(cpu == "paper"){
+            playerScore++;
+            cpuScore++;
             result = "It's a draw you both picked paper!";
         } else {
+            cpuScore++;
             result = "You lose! Scissors beats paper!";
         }
     } else {
         if(cpu == "rock"){
+            cpuScore++;
             result = "You lose! Rock beats scissors!";
         }else if(cpu == "paper"){
+            playerScore++;
             result = "You win! Scissors beats paper!";
         } else {
+            playerScore++;
+            cpuScore++;
             result = "It's a draw you both picked scissors!";
         }
     }
-    return result;
+    console.log(result);
+    printScore(playerScore, cpuScore);
+}
+function printScore(player, cpu){
+    console.log("The score is " + player + " to the player and " + cpu + " to the cpu");
+}
+
+function declareWinner(playerScore, cpuScore){
+    if(playerScore > cpuScore){
+        console.log("You wn the whole game!")
+    } else if (cpuScore > playerScore){
+        console.log("You lose, better luck next time!")
+    } else {
+        console.log("It's a draw!");
+    }
 }
 
 function playGame(){
     for (let i = 0; i < 5; i++) { 
         let computerSelection = getComputerChoice();
-        const playerSelection = prompt("Choose rock paper or scissors");
+        let playerSelection = prompt("Choose rock paper or scissors");
         playRound(playerSelection, computerSelection);
      }
+     declareWinner(playerScore, cpuScore);
 }

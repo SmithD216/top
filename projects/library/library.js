58c0ca99a1ee8591558100e78ad56096c109bf9b
D Smith <smithd216@protonmail.com>
2023-01-30 19:33:15 +0000

    removed table and created div system of displaying

--- projects/library/library.js
+++ projects/library/library.js
@@ -1,4 +1,4 @@
-
+let myLibrary = []
 
 function Book(author, title, pages, read){
     this.bookAuthor = author;
@@ -9,14 +9,26 @@ function Book(author, title, pages, read){
 
 function addBookToLibrary(event){
     const book = new Book(author.value, title.value, pages.value, read.checked);
-    console.log(book)
+    myLibrary.push(book);
+    displayLibrary();
     event.preventDefault();
 }
 
+function displayLibrary(){
+    listOfBooks.innerHTML = "";
+    for(book of myLibrary){
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `Author: ${book.bookAuthor} Title: ${book.bookTitle} Pages: ${book.bookPages} Read: ${book.bookRead}`
+        listOfBooks.appendChild(newDiv);
+    }
+}
+
+const table = document.getElementById('main-table');
 const author = document.getElementById('book-author');
 const title = document.getElementById('book-title');
 const pages = document.getElementById('book-pages');
 const read = document.getElementById('book-read');
+const listOfBooks = document.getElementById('list-of-books');
 
 const mainButton = document.getElementById('main-btn');
 mainButton.addEventListener('click', addBookToLibrary);

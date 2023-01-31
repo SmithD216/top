74d9a414c9d79df7424df76edb2a64f2633116ec
D Smith <smithd216@protonmail.com>
2023-01-31 18:04:16 +0000

    Added delete button

--- projects/library/library.js
+++ projects/library/library.js
@@ -21,9 +21,11 @@ function displayLibrary(){
         <th>Title</th>
         <th>Pages</th>
         <th>Read</th>
+        <th>Edit</th>
+        <th>Delete</th>
         </tr>
         `;
-    for(book of myLibrary){
+    for(const [index, book] of myLibrary.entries()){
         const newRow = table.insertRow()
         const authorCell = newRow.insertCell(0);
         authorCell.textContent = book.bookAuthor;
@@ -33,7 +35,19 @@ function displayLibrary(){
         pagesCell.textContent = book.bookPages;
         const readCell = newRow.insertCell(3);
         readCell.textContent = book.bookRead;
+        newRow.insertCell(4).innerHTML = "Edit";
+        newRow.insertCell(5).innerHTML = `<button type="button" data-index = ${index} onclick="deleteBook(this)">Delete</button>`;
     }
+}
+
+function  deleteBook(button){
+    let tableIndex = parseInt(button.dataset.index);
+    myLibrary.splice(tableIndex, 1);
+    tableIndex = tableIndex + 1;
+    console.log(tableIndex);
+    table.deleteRow(tableIndex);
+    displayLibrary();
+
 }
 
 const table = document.getElementById('main-table');

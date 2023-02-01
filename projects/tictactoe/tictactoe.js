6a66c5de0a62b6ae2a4394e8f5af5c4166853861
D Smith <smithd216@protonmail.com>
2023-02-01 16:11:44 +0000

    added identifiers to square

--- projects/tictactoe/tictactoe.js
+++ projects/tictactoe/tictactoe.js
@@ -1,32 +1,42 @@
 const gameBoard = () => {
-    let grid = ['X','O','X','X','O','X','O','X', 'O'];
-    const createBoard = () => {
+  const grid = ["X", "O", "X", "X", "O", "X", "O", "X", "O"];
+  const createBoard = () => {
         const gameboard = document.getElementById("gameboard");
-        for (let i = 0; i < 3; i++) {
-            let row = document.createElement('div');
-            row.classList.add('row');
-            gameboard.appendChild(row);
-            for (let j = 0; j < 3; j++) {
-                let square = document.createElement('div');
-                square.classList.add('square');
-                row.appendChild(square);   
+        const row1 = document.createElement("div");
+        row1.classList.add("row1");
+        const row2 = document.createElement("div");
+        row2.classList.add("row2");
+        const row3 = document.createElement("div");
+        row3.classList.add("row3");
+        for (const [index, value] of grid.entries()) {
+            const square = document.createElement('div');
+            square.setAttribute('id', `square-${index}`);
+            square.classList.add('square');
+            if(square.id == "square-0" || square.id == "square-1" || square.id == "square-2"){
+                row1.appendChild(square);
+            } else if (square.id == "square-3" || square.id == "square-4" || square.id == "square-5"){
+                row2.appendChild(square);
+            } else if (square.id == "square-6" || square.id == "square-7" || square.id == "square-8"){
+                row3.appendChild(square);
             }
-            
-        }
-    }
-    return { createBoard };
-}
+            gameboard.appendChild(row1);
+            gameboard.appendChild(row2);
+            gameboard.appendChild(row3);
+              
+        } 
+  };
+  return { createBoard };
+};
 
 const players = (symbol) => {
-    let turn = false;
-    const getSymbol = () => symbol;
+  let turn = false;
+  const getSymbol = () => symbol;
 
-    return { turn, getSymbol };
-}
+  return { turn, getSymbol };
+};
 
-const gameFlow = () => {
-
-}
+const gameFlow = () => {};
 
 const board = gameBoard();
+const game = gameFlow();
 board.createBoard();

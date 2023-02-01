6ec87d59ea9e1934cd2c4f82c9d145150c3b6805
D Smith <smithd216@protonmail.com>
2023-02-01 15:33:40 +0000

    Changed board generation to factory function

--- projects/tictactoe/tictactoe.js
+++ projects/tictactoe/tictactoe.js
@@ -1,15 +1,32 @@
-function createGameBoard() {
-    const gameboard = document.getElementById('gameboard');
-    for (let i = 0; i <3; i++) {
-        let row = document.createElement('div');
-        row.classList.add('row');
-        gameboard.appendChild(row);
-        for (let i = 0; i <3; i++){
-            let square = document.createElement('div');
-            square.classList.add('square');
-            row.appendChild(square);
+const gameBoard = () => {
+    let grid = ['X','O','X','X','O','X','O','X', 'O'];
+    const createBoard = () => {
+        const gameboard = document.getElementById("gameboard");
+        for (let i = 0; i < 3; i++) {
+            let row = document.createElement('div');
+            row.classList.add('row');
+            gameboard.appendChild(row);
+            for (let j = 0; j < 3; j++) {
+                let square = document.createElement('div');
+                square.classList.add('square');
+                row.appendChild(square);   
+            }
+            
         }
     }
+    return { createBoard };
 }
 
-createGameBoard();
+const players = (symbol) => {
+    let turn = false;
+    const getSymbol = () => symbol;
+
+    return { turn, getSymbol };
+}
+
+const gameFlow = () => {
+
+}
+
+const board = gameBoard();
+board.createBoard();

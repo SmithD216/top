253dda6cbc21693e29ee874da5eed77c8ced1dfd
D Smith <smithd216@protonmail.com>
2023-03-15 12:34:27 +0000

    Set up first export/import.

---
+++ projects/todo/scripts/dom.js
@@ -0,0 +1,4 @@
+export function createContainer() {
+    const maindiv = document.querySelector(".maindiv");
+    maindiv.appendChild(document.createElement("p"));
+}
---
+++ projects/todo/scripts/main.js
@@ -0,0 +1,2 @@
+import { createContainer } from "./dom.js";
+createContainer();
